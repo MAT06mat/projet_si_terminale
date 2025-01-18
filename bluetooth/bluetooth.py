90a348c56f3384398848c37309709511c8a06f7c
Matthieu <matthieu.felten22@gmail.com>
2025-01-18 18:02:53 +0100

    Enhance Bluetooth class with improved connection handling and request processing

--- bluetooth/bluetooth.py
+++ bluetooth/bluetooth.py
@@ -6,10 +6,15 @@ dotenv.load_dotenv()
 
 ADRESSE = os.getenv("ADRESSE")
 PORT = os.getenv("PORT")
+REQUEST_LENGHT = 512
+
+
+Request.REQUEST_LENGHT = REQUEST_LENGHT
 
 
 class Bluetooth:
     public_vars = ["callback", "a", "b", "c", "d"]
+    is_server = False
 
     def __init__(self):
         self.socket = socket.socket(
@@ -17,7 +22,6 @@ class Bluetooth:
         )
         self.client = None
         self.connected = False
-        self.is_server = False
         self.is_server_connected = False
 
         self.a = 1
@@ -25,65 +29,40 @@ class Bluetooth:
         self.c = "test"
         self.d = "^ç^"
 
-    def client_connect(self):
-        if self.client:
-            return
-        self.socket.connect((ADRESSE, int(PORT)))
-        self.client = self.socket
-        print("client-connected")
-        threading.Thread(target=self.loop).start()
-
-    def server_connect(self):
-        if self.is_server_connected:
-            return
-        self.is_server = True
-        self.is_server_connected = True
-        self.socket.bind((ADRESSE, int(PORT)))
-        self.socket.listen(1)
-        print("server-online")
-        threading.Thread(target=self.loop).start()
-
-    def client_deconnect(self):
-        self.send(Request.encode({"DECONNECT": 1}))
-        self.client.close()
-
-    def server_deconnect(self):
-        if self.is_server_connected:
-            self.is_server_connected = False
-            if self.client:
-                self.client.close()
-            self.socket.close()
-
     def loop(self):
-        while self.is_server_connected or self.client:
+        print("Loop started")
+        buffer = b""
+        while True:
             try:
                 if not self.client and self.is_server:
                     self.client, addr = self.socket.accept()
-                data = self.client.recv(4096)
-            except (ConnectionAbortedError, KeyboardInterrupt, OSError) as e:
-                self.server_deconnect()
+                buffer += self.client.recv(REQUEST_LENGHT * 8)
+            except (ConnectionAbortedError, OSError) as e:
+                if self.is_server_connected:
+                    self.client = None
+                    continue
+                print("Loop stopped")
                 return
-            json = Request.decode(data)
 
-            if "CALL" in json:
-                if json["CALL"]["fname"] in self.public_vars:
-                    func = self.__getattribute__(json["CALL"]["fname"])
-                    func(*json["CALL"]["args"])
+            while len(buffer) >= REQUEST_LENGHT:
+                json = Request.decode(buffer[:REQUEST_LENGHT])
+                buffer = buffer[REQUEST_LENGHT:]
 
-            if "GET" in json:
-                if json["GET"]["var"] in self.public_vars:
-                    fid = json["GET"]["fid"]
-                    value = self.__getattribute__(json["GET"]["var"])
-                    request = Request.call("callback", fid, value)
-                    self.send(request)
+                if "CALL" in json:
+                    if json["CALL"]["fname"] in self.public_vars:
+                        func = self.__getattribute__(json["CALL"]["fname"])
+                        func(*json["CALL"]["args"])
 
-            if "SET" in json:
-                if json["SET"]["var"] in self.public_vars:
-                    self.__setattr__(json["SET"]["var"], json["SET"]["value"])
+                if "GET" in json:
+                    if json["GET"]["var"] in self.public_vars:
+                        fid = json["GET"]["fid"]
+                        value = self.__getattribute__(json["GET"]["var"])
+                        request = Request.call("callback", fid, value)
+                        self.send(request)
 
-            if "DECONNECT" in json:
-                self.client.close()
-                self.client = None
+                if "SET" in json:
+                    if json["SET"]["var"] in self.public_vars:
+                        self.__setattr__(json["SET"]["var"], json["SET"]["value"])
 
     def callback(self, *args):
         Request.callback(*args)
@@ -93,3 +72,38 @@ class Bluetooth:
             self.client.send(request)
         except ConnectionResetError:
             pass
+
+
+class Client(Bluetooth):
+    is_server = False
+
+    def connect(self):
+        if self.client:
+            return
+        self.socket.connect((ADRESSE, int(PORT)))
+        self.client = self.socket
+        print("client-connected")
+        threading.Thread(target=self.loop).start()
+
+    def deconnect(self):
+        self.client.close()
+
+
+class Server(Bluetooth):
+    is_server = True
+
+    def connect(self):
+        if self.is_server_connected:
+            return
+        self.is_server_connected = True
+        self.socket.bind((ADRESSE, int(PORT)))
+        self.socket.listen(1)
+        print("server-online")
+        threading.Thread(target=self.loop).start()
+
+    def deconnect(self):
+        if self.is_server_connected:
+            self.is_server_connected = False
+            if self.client:
+                self.client.close()
+            self.socket.close()
--- bluetooth/request.py
+++ bluetooth/request.py
@@ -2,26 +2,30 @@ import json
 
 
 class Request:
-    callbacks = {}
+    callbacks = []
+    REQUEST_LENGHT = 64
 
     def call(fname, *args):
         return Request.encode({"CALL": {"fname": fname, "args": args}})
 
     def get(var, callback):
         fid = id(callback)
-        Request.callbacks[fid] = callback
+        Request.callbacks.append((fid, callback))
         return Request.encode({"GET": {"var": var, "fid": fid}})
 
     def set(var, value):
         return Request.encode({"SET": {"var": var, "value": value}})
 
     def encode(obj):
-        return json.dumps(obj).encode("utf-8")
+        binary = json.dumps(obj).encode("utf-8")
+        fill = Request.REQUEST_LENGHT - len(binary)
+        return binary + b"\x00" * fill
 
     def decode(obj):
-        return json.loads(obj.decode("utf-8"))
+        return json.loads(obj.rstrip(b"\x00").decode("utf-8"))
 
     def callback(fid, value):
-        callback = Request.callbacks[fid]
-        del Request.callbacks[fid]
-        return callback(value)
+        for i in Request.callbacks:
+            if i[0] == fid:
+                Request.callbacks.remove(i)
+                return i[1](value)
