9fc6c12ec9ad91b01196f0ffa0afc782c8023a55
Matthieu <matthieu.felten22@gmail.com>
2025-01-19 17:44:22 +0100

    Update compilation script

--- client/compilation.py
+++ client/compilation.py
@@ -5,28 +5,29 @@ from colorama import init, Fore, Style
 # Init Colorama
 init(autoreset=True)
 
-
+# Raise error if script isn't launch on linux
 if sys.platform not in ("linux", "linux2"):
     raise Exception("Bad platform, try on linux")
 
+# Update code on vm
+os.system("rm -rf bin")
+os.system("rm -rf app")
+print("Update ...")
+os.system("mkdir app")
+os.system("cp -R /mnt/rcm/app/. app/.")
+os.system("rm -rf buildozer.spec")
+os.system("cp -R /mnt/rcm/buildozer.spec buildozer.spec")
+print("Update of buildozer.spec and app/ completed")
 
-commands = (
-    "rm -rf bin",
-    "rm -rf app",
-    'echo "Update..."',
-    "mkdir app",
-    "cp -R /mnt/rcm/app/. app/.",
-    "rm -rf buildozer.spec",
-    "cp -R /mnt/rcm/buildozer.spec buildozer.spec",
-    'echo "Update of buildozer.spec and app/ completed"',
-    "buildozer --verbose android debug",
-)
-
-for command in commands:
-    os.system(command)
-
+# Start buildozer compilation
+os.system("buildozer --verbose android debug")
 from app.main import __version__
 
-print(
-    f"\n\n{Style.BRIGHT}{Fore.GREEN}#     Compilation finished !\n{Fore.BLACK}#     File rcm-{__version__}-armeabi-v7a_arm64-v8a-debug.apk is available in /bin directory !"
-)
+print(f"\n\n{Style.BRIGHT}{Fore.GREEN}#     Compilation finished !\n{Fore.BLACK}#")
+
+rep = input("Do you want a copy out of the vm ? (y/n)")
+if rep.lower() == "y":
+    os.system(
+        f"cp -R ./bin/rcm-{__version__}-armeabi-v7a_arm64-v8a-debug.apk /mnt/rcm/rcm-{__version__}-armeabi-v7a_arm64-v8a-debug.apk"
+    )
+    print("Copy done !")
